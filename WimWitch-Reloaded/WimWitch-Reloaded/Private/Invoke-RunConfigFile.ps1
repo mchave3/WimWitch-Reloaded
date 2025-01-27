97cbaaf4d448217637d8e4d39170d51f1a8b96ae
Mickaël CHAVE <mchave3@live.fr>
2025-01-27 17:09:05 +0100

    Add new PowerShell functions for displaying closing messages, selecting configuration files, running config files, resetting checkboxes, and retrieving Autopilot profiles

---
+++ WimWitch-Reloaded/WimWitch-Reloaded/Private/Invoke-RunConfigFile.ps1
@@ -0,0 +1,41 @@
+<#
+.SYNOPSIS
+    Load the configuration file and run the MakeItSo function.
+
+.DESCRIPTION
+    This function is used to load the configuration file and run the MakeItSo function.
+
+.NOTES
+    Name:        Invoke-RunConfigFile.ps1
+    Author:      Mickaël CHAVE
+    Created:     2025-01-27
+    Version:     1.0.0
+    Repository:  https://github.com/mchave3/WimWitch-Reloaded
+    License:     MIT License
+
+    Based on original WIM-Witch by TheNotoriousDRR :
+    https://github.com/thenotoriousdrr/WIM-Witch
+
+.LINK
+    https://github.com/mchave3/WimWitch-Reloaded
+
+.EXAMPLE
+    Invoke-RunConfigFile -filename $filename
+#>
+function Invoke-RunConfigFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$filename
+    )
+
+    process {
+        Update-Log -Data "Loading the config file: $filename" -Class Information
+        Get-Configuration -filename $filename
+        Update-Log -Data $WWScriptVer
+        Invoke-MakeItSo -appx $global:SelectedAppx
+        Write-Output ' '
+        Write-Output '##########################################################'
+        Write-Output ' '
+    }
+}
