5f43af70d25a3a1486636c4babcfdce2a1c56517
Mickaël CHAVE <mchave3@live.fr>
2025-01-31 20:57:25 +0100

    Add Install-LocalExperiencePack function for applying Local Experience Packs to mounted WIM

--- docs/template.ps1
+++ docs/template.ps1
@@ -8,7 +8,7 @@
 .NOTES
     Name:        ScriptName.ps1
     Author:      Mickaël CHAVE
-    Created:     2025-01-30
+    Created:     2025-01-31
     Version:     1.0.0
     Repository:  https://github.com/mchave3/WimWitch-Reloaded
     License:     MIT License
