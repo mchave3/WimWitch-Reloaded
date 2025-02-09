615c610d581a6585eea28a1185898755e9786882
Mickaël CHAVE <mchave3@live.fr>
2025-02-09 17:07:35 +0100

    🔧 [Enhancement]: Add PSScriptAnalyzer settings and update .gitignore to include settings file
    🔧 [Patch]: Fix all Linter errors #10

---
+++ .vscode/PSScriptAnalyzerSettings.psd1
@@ -0,0 +1,20 @@
+#Documentation: https://github.com/PowerShell/PSScriptAnalyzer/blob/master/docs/Cmdlets/Invoke-ScriptAnalyzer.md#-settings
+@{
+    #CustomRulePath='path\to\CustomRuleModule.psm1'
+    #RecurseCustomRulePath='path\of\customrules'
+    Severity = @(
+        'Error',
+        'Warning'
+    )
+    #IncludeDefaultRules=${true}
+    ExcludeRules = @(
+        'PSMissingModuleManifestField',
+        'PSAvoidUsingWriteHost',
+        'PSAvoidUsingInvokeExpression',
+        'PSAvoidUsingWriteHost'
+    )
+    #IncludeRules = @(
+    #    'PSAvoidUsingWriteHost',
+    #    'MyCustomRuleName'
+    #)
+}
--- src/functions/private/Backup-WIMWitch.ps1
+++ src/functions/private/Backup-WIMWitch.ps1
@@ -34,7 +34,6 @@ function Backup-WIMWitch {
         $scriptname = Split-Path $MyInvocation.PSCommandPath -Leaf #Find local script name
         Update-Log -data 'The script to be backed up is: ' -Class Information
         Update-Log -data $MyInvocation.PSCommandPath -Class Information
-    
         try {
             Update-Log -data 'Copy script to backup folder...' -Class Information
             Copy-Item -Path $scriptname -Destination $Script:workdir\backup -ErrorAction Stop
@@ -44,13 +43,11 @@ function Backup-WIMWitch {
             Update-Log -Data 'Exiting out of an over abundance of caution' -Class Error
             exit
         }
-    
         try {
             Update-Log -data 'Renaming archived script...' -Class Information
             Rename-Name -file $Script:workdir\backup\$scriptname -extension '.ps1'
             Update-Log -data 'Backup successfully renamed for archiving' -class Information
         } catch {
-    
             Update-Log -Data "Backed-up script couldn't be renamed. This isn't a critical error" -Class Warning
             Update-Log -Data "You may want to change it's name so it doesn't get overwritten." -Class Warning
             Update-Log -Data 'Continuing with WIM Witch upgrade...' -Class Warning
