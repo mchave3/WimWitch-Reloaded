c8f4c6864d54b7ed76e9cbcd88a4badebe48cd01
Mickaël CHAVE <mchave3@live.fr>
2025-02-25 17:06:54 +0100

    🔧 [Update]: Remove Select-WWWorkingDirectory.ps1 and add new implementations in Core
    #20

---
+++ src/functions/private/Core/Invoke-WWWorkingDirectory.ps1
@@ -0,0 +1,50 @@
+﻿<#
+.SYNOPSIS
+    
+
+.DESCRIPTION
+    
+
+.NOTES
+    Name:        Invoke-WWWorkingDirectory.ps1
+    Author:      Mickaël CHAVE
+    Created:     2025-02-25
+    Version:     1.0.0
+    Repository:  https://github.com/mchave3/WimWitch-Reloaded
+    License:     MIT License
+
+    Based on original WIM-Witch by TheNotoriousDRR :
+    https://github.com/thenotoriousdrr/WIM-Witch
+
+.LINK
+    https://github.com/mchave3/WimWitch-Reloaded
+
+.EXAMPLE
+    Invoke-WWWorkingDirectory
+#>
+function Invoke-WWWorkingDirectory {
+    [CmdletBinding()]
+    param(
+
+    )
+
+    process {
+        $Script:workdir = $env:ProgramData + '\WimWitch-Reloaded'
+
+        $subfolders = @(
+            'CompletedWIMs'
+            'Configs'
+            'Drivers'
+            'Jobs'
+            'Logging'
+            'Mount'
+            'Staging'
+            'Updates'
+            'Imports'
+            'Imports\WIM'
+            'Imports\DotNet'
+            'Autopilot'
+            'Backup'
+        )
+    }
+}
