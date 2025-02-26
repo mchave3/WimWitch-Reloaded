db55bb43ffdbaf7b873d5a14dccccb6cea6effed
Mickaël CHAVE <mchave3@live.fr>
2025-02-26 12:35:21 +0100

    🔧 [Update]: Add variable directory processing to Build_module.ps1 and log variable file additions in Invoke-WWWorkingDirectory
    🚀 [Feature]: Automate Working Directory Definition #20

--- src/functions/private/Core/Invoke-WWWorkingDirectory.ps1
+++ src/functions/private/Core/Invoke-WWWorkingDirectory.ps1
@@ -49,6 +49,8 @@ function Invoke-WWWorkingDirectory {
                 'Backup'
             )
 
+            Write-Output $script:workingDirectory
+
             # Check that $script:workingDirectory is defined
             if (-not $script:workingDirectory) {
                 throw "Working directory is not defined"
